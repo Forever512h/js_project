a1a3ca2766dab3f1143ea5e2632cc29895344581
Elmurod <elmurodjuraev22@gmail.com>
2022-11-17 11:48:19 +0500

    Upd: logic and math

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-//new file
+let c = 10;
+let d = 35;
+if(c == d)
+  alert("Значение переменной c равно d");
+else
+  alert("Значение переменной c не равно d");
+
+let a = 'qwerty';
+let b = 'qwerty';
+  if(a == b)
+    alert("Значение переменной a равно b");
+  else
+    alert("Значение переменной a не равно b");
+
+ a = 'Qwerty';
+ b = 'qwerty';
+ if(a != b)
+  alert("Значение переменной a не равно b");
+ else
+ alert("Значение переменной a равно b");
+
+ c = 20;
+ d = 20;
+ if(a != b || c != d)
+  alert("Значение переменной a не равно b");
+ else
+ alert("Значение переменной c равно d");
